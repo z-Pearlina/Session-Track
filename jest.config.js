348ec9295ca5f15381d7b5b1955ec3faac157194
z-Pearlina <pearlinaofficial@gmail.com>
2025-11-09 19:50:40 +0100

    config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.d.ts',
